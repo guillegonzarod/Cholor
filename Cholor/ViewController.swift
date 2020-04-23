d5741e5a4323fd6dd6aefaaeab72aa69f0d4db77
Guille <info@guillegonzarod.com>
2020-04-23 20:36:34 +0200

    Initial Commit

---
+++ Cholor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cholor
+//
+//  Created by Guillermo González Rodríguez on 23/4/20.
+//  Copyright © 2020 Guillermo González Rodríguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
